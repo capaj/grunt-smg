73bfde05138ffade082a74effa4b6cca33ac440a
Jiri Spac <capajj@gmail.com>
2013-08-28 11:43:57 +0200

    fixed a lot of stuff that was wrong since I did not have time to test it yesterday

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,58 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
-    smg: {
-      default_options: {
-        options: {
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
         },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
+
+        // Configuration to be run (and then tested).
+        smg: {
+            test:{
+                steps: {
+                    1: ['test/public/*.js'],
+                    2: ['test/public/*.js'],
+                    3: ['test/public/*.js']
+                },
+                dest: 'test/public/modules-definitions.js'
+            }
         },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
+        }
 
-  });
+    });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'smg', 'nodeunit']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['smg']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/smg.js
+++ tasks/smg.js
@@ -18,29 +18,38 @@ module.exports = function (grunt) {
         var data = this.data;
         var output = '$script([';   //beginning
         var counter = 0;
+        grunt.log.writeln(JSON.stringify(data));
 
         for(var step in data.steps){
-            grunt.log.write('Processing step '+ step +': ');
+            grunt.log.writeln('Processing step '+ step +': ');
             var forGlob = data.steps[step];
+            grunt.log.writeln(JSON.stringify(forGlob));
+
             forGlob.forEach(function (globExp) {
                 glob(globExp, function (er, files) {
+                    grunt.log.writeln(JSON.stringify(files));
+
                     var fileName;
                     while(fileName = files.pop()){
                         output += fileName + ',';
                     }
                 });
             });
-            if (data.steps[step + 1]) {
+            if (data.steps[Number(step) + 1]) {
                 output += '], function() { $script([';
                 counter += 1;
-            } else {
+            } else {    //when the last should run
                 output += '], "scriptManifestReady");';
             }
+        }
+        if (counter == 0) {
+            output += '], "scriptManifestReady");';
         }
         while(counter--){
             output += '});';
         }
-        grunt.file.write('scriptManifest.js', output);
+        var dest = data.dest || 'scriptManifest.js';
+        grunt.file.write(dest, output);
         grunt.log.writeln('Script manifest has been written.');
     });
 
